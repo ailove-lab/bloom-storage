831b5d274155aa7660f672de3b021d22e6a63551
peko <peko@gasubasu.com>
2015-11-10 02:32:54 +0300

    random

---
+++ src/entry-generator/random-key-generator-multi.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"math/rand"
+	"runtime"
+	"strconv"
+	"time"
+	"sync"
+)
+
+var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+
+func main() {
+
+
+
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	flag.Parse()
+	args := flag.Args()
+
+	cnt, err := strconv.Atoi(args[0])
+	if err != nil || cnt <= 0 {
+		fmt.Println("usage: generator count", err)
+		return
+	}
+	var wg sync.WaitGroup
+	wg.Add(24)
+	for j :=0; j<24; j++ {
+		go func(){	
+			for i := 0; i < cnt/24; i++ {
+				fmt.Printf("%08X%08X%08X%08X\n",rnd.Uint32(),rnd.Uint32(),rnd.Uint32(),rnd.Uint32())
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+
+}
