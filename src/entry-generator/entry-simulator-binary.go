31f1471b75490e430fcedca4c5ae6c970fa4d134
peko <peko@gasubasu.com>
2015-11-09 19:10:22 +0300

    entry simulation

---
+++ src/entry-generator/entry-simulator-binary.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("../../data/data-sample")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		key_val := strings.Split(scanner.Text(), "\t")
+		if len(key_val) == 2 {
+			fmt.Print("LPUSH ", key_val[0], " \"", key_val[1], "\"\r\n")
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
