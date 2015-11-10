14b2182cd4f0304e2bc6f7989d7abdb6589a872f
peko <peko@gasubasu.com>
2015-11-10 20:49:10 +0300

    update

--- src/binary-loader-c/binary-loader.c
+++ src/binary-loader-c/binary-loader.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "bstrlib.h"
 
 int main() {
     char *line = NULL;
     size_t size;
     int i=0;
-    while(getline(&line, &size, stdin) > 0) {
+    while(getline(&line, &size, stdin) != -1) {
+        bstring bline = bfromcstr(line);
+
         printf("%d:%s", i++, line);
+
+        bdestroy(bline);
     }
+    free(line);
     return 0;
 }
