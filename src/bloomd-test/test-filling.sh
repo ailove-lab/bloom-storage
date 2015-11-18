cb3fd8a0107fcf3f12367495fb547320343cec39
peko <peko@gasubasu.com>
2015-11-18 08:13:05 +0400

    update

--- src/bloomd-test/test-filling.sh
+++ src/bloomd-test/test-filling.sh
@@ -4,7 +4,12 @@ printf "\n--- create b\n"
 echo create b | nc localhost 8673
 du -sh $file
 printf "\n--- fill\n"
-time cat $file | paste -s -d ' ' | sed -e 's/^/b b /' | nc localhost 8673 >/dev/null
+#time cat $file | nc localhost 8673 >/dev/null
+#time cat $file | paste -s -d ' ' | sed -e 's/^/b b /' | nc localhost 8673 >/dev/null
+#time cat $file | sed -e 's/^/s b /' | nc localhost 8673 >/dev/null
+time head -n 1000 $file | sed -e 's/^/s b /' 
+#>/dev/null
+
 printf "\n--- list\n"
 echo list | nc localhost 8673
 printf "\n--- info b\n"
